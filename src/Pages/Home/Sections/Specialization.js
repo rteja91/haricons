a79e46dba36103963f1a71acdd124433d4a2059e
Ravi Teja Gangineni <rteja7228@gmail.com>
2019-01-27 13:17:54 +0530

    fixed text stuff

--- src/Pages/Home/Sections/Specialization.js
+++ src/Pages/Home/Sections/Specialization.js
@@ -4,11 +4,11 @@ import { MDBRow, MDBCol, MDBIcon, MDBBtn } from "mdbreact";
 const Specialization = () => {
     return (
         <section className="my-5">
-            <h2 className="h1-responsive font-weight-bold text-center my-5">
+            <h2 className="h1-responsive font-weight-bold text-center my-3">
                 Why Haricons?
       </h2>
             <h3 className="text-center my-2 font-weight-bold">WE EXCEL AND MAKE OUR MARK IN ALL OUR WORKS</h3>
-            <p className="lead grey-text w-responsive text-center mx-auto mb-5">
+            <p className="lead  w-responsive text-center mx-auto mb-5">
 
                 <br></br>
                 We are a diversified group with  following areas of expertise:
